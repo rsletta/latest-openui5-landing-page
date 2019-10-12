821b30d9fbfdab2cb6fbf6b2252c62ce1aa472d7
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2019-10-13 00:05:14 +0200

    Insert firebase config + init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+import firebase from 'firebase';
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.config.ignoredElements = [/^ui5-/];
+
+const config = {
+  apiKey: "AIzaSyADelwnX6NDcpLZZENKA3DpqZL2liDK1aE",
+  databaseURL: "https://get-latest-openui5-version.firebaseio.com",
+  projectId: "get-latest-openui5-version"
+};
+
+firebase.initializeApp(config);
 
 new Vue({
   render: h => h(App),
