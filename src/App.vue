d44d3df6cdcc86e23ab6998bfff6b17ab5a97dcd
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2019-10-13 00:06:05 +0200

    Add shell header

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,51 @@
 <template>
-  <div id="app">
-    <LandingPage></LandingPage>
+  <div class="app">
+    <header class="app-header">
+      <ui5-shellbar profile></ui5-shellbar>
+    </header>
+    <section></section>
+    <VersionList />
   </div>
 </template>
 
 <script>
-import LandingPage from "./components/LandingPage.vue";
+import "@ui5/webcomponents-base/dist/features/browsersupport/Edge";
+import VersionList from "./components/VersionList.vue";
+import "@ui5/webcomponents/dist/ShellBar";
 
 export default {
   name: "app",
   components: {
-    LandingPage
+    VersionList
   }
 };
 </script>
 
 <style>
-#app {
+body {
+  margin: 0;
+}
+
+.app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+
+.ui5-logo {
+  height: 2rem;
+  margin-left: 2rem;
+}
+.app-title {
+  margin-left: 1rem;
+}
+
+.app-content {
+  height: calc(100% - 3rem);
+  padding: 0 1rem;
+  width: calc(100% - 2rem);
 }
 </style>
