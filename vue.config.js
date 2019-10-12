37e58140c95b631bf209ca04382cf5555f45e251
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2019-10-13 00:05:27 +0200

    add vue.config

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    configureWebpack: {
+        module: {
+            rules: [{
+                test: [/cldr\/.*\.json$/, /i18n\/.*\.json$/],
+                loader: 'file-loader',
+                options: {
+                    name: 'static/media/[name].[hash:8].[ext]',
+                },
+                type: 'javascript/auto'
+            }]
+        }
+    }
+};
