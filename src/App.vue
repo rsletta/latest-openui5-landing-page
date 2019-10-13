6404ff4d943f87b23655abc7f143bafc71b896fa
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2019-10-13 15:49:05 +0200

    Add content

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,69 @@
 <template>
   <div class="app">
     <header class="app-header">
-      <ui5-shellbar profile></ui5-shellbar>
+      <ui5-shellbar
+        id="shellbar"
+        logo="https://img.shields.io/github/package-json/v/rsletta/get_openui5_latest_url.svg"
+        @logoClick="onLogoClick"
+      >
+      </ui5-shellbar>
     </header>
-    <section></section>
-    <VersionList />
+    <section class="app-content">
+      <img
+        class="header-img"
+        src="./assets/repository-header.png"
+        alt="OpenUI5 Latest Version - Community API header"
+      />
+      <div class="card-container">
+        <div class="bodytext">
+          <p>
+            <ui5-link href="https://openui5.org/" target="_blank">OpenUI5</ui5-link> Latest Version is a community run API, serving the urls of the latest
+            available
+            versions from
+            <ui5-link href="https://github.com/sap/openui5" target="_blank">Github</ui5-link>.
+          </p>
+          <ui5-title>Documentation:</ui5-title>
+          <p>
+            Documentation for the API is available on
+            <ui5-link
+              href="https://github.com/rsletta/get_openui5_latest_url"
+              target="_blank"
+            >Github</ui5-link>.
+          </p>
+          <ui5-title>Disclaimer:</ui5-title>
+          <p>
+            This service has no affiliation to
+            <ui5-link href="https://www.sap.com" target="_blank">SAP AG</ui5-link> or the
+            <ui5-link href="https://openui5.org/" target="_blank">OpenUI5</ui5-link>.
+          </p>
+        </div>
+        <div>
+        <ui5-title>What are the newest versions?</ui5-title>
+        <ui5-card class="medium">
+          <div class="card-content">
+            <VersionList />
+          </div>
+        </ui5-card>
+        </div>
+      </div>
+    </section>
+    <footer class="footer">
+      <div>
+        <p>
+          Created with ♥️ by
+          <ui5-link href="https://twitter.com/rsletta" target="_blank">
+            Ronnie André Bjørvik
+            Sletta
+          </ui5-link>. Powered by
+          <ui5-link href="https://vuejs.org" target="_blank">Vue.js</ui5-link>,
+          <ui5-link href="https://firebase.google.com/" target="_blank">Firebase</ui5-link>, and
+          <ui5-link
+            href="https://sap.github.io/ui5-webcomponents/"
+            target="_blank"
+          >UI5 Web Components</ui5-link>.
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -12,11 +71,21 @@
 import "@ui5/webcomponents-base/dist/features/browsersupport/Edge";
 import VersionList from "./components/VersionList.vue";
 import "@ui5/webcomponents/dist/ShellBar";
+import "@ui5/webcomponents/dist/ShellBarItem";
+import "@ui5/webcomponents/dist/Icon";
+import "@ui5/webcomponents/dist/Card";
+import "@ui5/webcomponents/dist/Title";
+import "@ui5/webcomponents/dist/Link";
 
 export default {
   name: "app",
   components: {
     VersionList
+  },
+  methods: {
+    onLogoClick: function() {
+      window.open("https://github.com/rsletta/get_openui5_latest_url");
+    }
   }
 };
 </script>
@@ -47,5 +116,32 @@ body {
   height: calc(100% - 3rem);
   padding: 0 1rem;
   width: calc(100% - 2rem);
+}
+
+.bodytext {
+  margin-top: 30px;
+  padding: 0 1rem;
+  text-align: start;
+  font-size: var(--sapMFontMediumSize, 0.875rem);
+}
+
+.footer  {
+  font-size: var(--sapMFontMediumSize, 0.875rem);
+}
+ui5-card {
+  margin: 1rem;
+  min-width: 18rem;
+}
+
+ui5-card.medium {
+  width: 20rem;
+}
+
+.card-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  min-width: 16rem;
 }
 </style>
