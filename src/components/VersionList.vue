8f84aafdac1d9d83605753bd89759364df5ee645
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2019-10-13 16:21:56 +0200

    refactor busy indicator

--- src/components/VersionList.vue
+++ src/components/VersionList.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <div v-if="listReady">
-      <ui5-list
-        v-for="version in versions"
-        v-bind:key="version.version"
-        id="myList"
-        class="card-content-child list"
-        style="width: 100%"
-        separators="None"
-      >
-        <ui5-li type="Inactive" :info="version.eom" :info-state="version.lts">{{ version.version }}</ui5-li>
-      </ui5-list>
-    </div>
-    <div v-else>
-      <ui5-busyindicator active size="Large"></ui5-busyindicator>
-    </div>
+    <ui5-busyindicator :active="!listReady" size="Large">
+      <div>
+        <ui5-list
+          v-for="version in versions"
+          v-bind:key="version.version"
+          id="myList"
+          class="card-content-child list"
+          style="width: 100%"
+          separators="None"
+        >
+          <ui5-li
+            type="Inactive"
+            :info="version.eom"
+            :info-state="version.lts"
+          >{{ version.version }}</ui5-li>
+        </ui5-list>
+      </div>
+    </ui5-busyindicator>
   </div>
 </template>
 
 <script>
-import firebase from "firebase";
+import firebase from "firebase/app";
+import 'firebase/firestore';
 import "@ui5/webcomponents/dist/BusyIndicator";
 import "@ui5/webcomponents/dist/List";
 import "@ui5/webcomponents/dist/StandardListItem";
@@ -39,7 +43,7 @@ export default {
         snapshot.forEach(doc => {
           if (doc.id === "latest" || doc.id === "savedVersions") {
             return;
-          } 
+          }
           let newEntry = {};
           let data = doc.data();
           newEntry.version = data.version;
@@ -50,18 +54,18 @@ export default {
           newEntry.lts = data.lts ? "Success" : "Neutral";
           newEntry.eom = data.eom ? data.eom : "";
           this.versions.push(newEntry);
-        })
+        });
 
         this.versions.reverse();
         this.listReady = true;
-      })
+      });
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.list{
+.list {
   text-align: start;
 }
 </style>
