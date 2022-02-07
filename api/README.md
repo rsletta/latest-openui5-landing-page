c94a8bb702433fd341f421d046f50709afb51fb3
Ronnie André Bjørvik Sletta <rsletta@gmail.com>
2022-02-07 16:32:45 +0100

    API implementation

--- api/README.md
+++ api/README.md
@@ -3,9 +3,6 @@
 ![GitHub package.json version (subfolder of monorepo)](https://img.shields.io/github/package-json/v/rsletta/latest-openui5-landing-page?filename=api%2Fpackage.json)
 [![GitHub license](https://img.shields.io/github/license/rsletta/get_openui5_latest_url.svg)](https://github.com/rsletta/get_openui5_latest_url/blob/master/LICENSE)
 
-
-
-
 A small webservice that returns url to latest OpenUI5 version. Hosted at https://latest-openui5.rikosjett.com. Version URLs are collected from https://openui5.org/releases/. This API only serves URLs to the official downloads. API call with only type specified, returns the url as a string. If you leave out query options, it defaults to return the runtime URL. When adding format=json, it returns a richer response.
 
 ## Endpoints
@@ -37,30 +34,6 @@ Data example
     "type": "runtime",
     "url": "https://openui5.hana.ondemand.com/downloads/openui5-runtime-1.65.1.zip"
 }
-```
-
-#### Error Responses
-
-**Type**: Document not found
-
-**Code** `404`
-
-**Returns**: `text/html`
-
-Data example
-```
-Document not found
-```
-
-**Type**: Missing query options
-
-**Code**: `400`
-
-**Returns**: `text/html`
-
-Data example
-```
-Missing type query.
 ```
 
 ### Get latest major.minor
@@ -100,18 +73,7 @@ Data example
 
 Data example
 ```
-Document not found
-```
-
-**Type**: Missing query options
-
-**Code**: `400`
-
-**Returns**: `text/html`
-
-Data example
-```
-Missing type query.
+Version not found in db. Version: X.X
 ```
 
 ## Disclaimer
--- api/latest-openui5-webApi/index.ts
+++ api/latest-openui5-webApi/index.ts
@@ -1,14 +1,93 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.', context, req.query);
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = `${context.bindingData.id}  ${req.query}`
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    context.log('Starting API request processing');
 
+    // Setup
+    let idParam = null;
+    let response = null;
+
+    // Move JSON data from storage blob for readability
+    const versionJSON = context.bindings.inputBlob;
+    const queryParams = req.query;
+    const type = queryParams.type || "runtime";
+
+    if (context.bindingData.id) {
+        idParam = context.bindingData.id;
+        context.log(`Request has parameter: ${idParam}`);
+    }
+
+    // If theres no id parameter, return latest 
+    if (idParam === null) {
+        if (queryParams.format === 'json') {
+            response = {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                status: 200,
+                body: {
+                    'version': versionJSON['latest'].version,
+                    'type': type,
+                    'url': versionJSON['latest'][type]
+                }
+            }
+        } else {
+            response = {
+                status: 200,
+                body: versionJSON['latest'][type]
+            };
+        }
+    } else {
+        // There is a id here, check if it's a saved version
+        const queriedVersion = versionJSON.versions[idParam];
+        if (queriedVersion) {
+            // We have the version saved
+            if (queryParams.format === 'json') {
+                response = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    status: 200,
+                    body: {
+                        'version': queriedVersion.version,
+                        'type': type,
+                        'url': queriedVersion[type]
+                    }
+                }
+            } else {
+                response = {
+                    status: 200,
+                    body: queriedVersion[type]
+                };
+            }
+        } else {
+            // Version is not available
+            context.log(`Version not found in db. Version: ${idParam}`);
+            response = {
+                headers: {
+                    'Content-Type': 'text/html'
+                },
+                status: 404,
+                body: `Version not found in db. Version: ${idParam}`
+            };
+        }
+    }
+
+    // Catch all, if something slips through. It should never trigger this.
+    if (response === null) {
+        response = {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            status: 400,
+            body: {
+                message: `Something is wrong. Please check the documentation.`,
+                url: 'https://github.com/rsletta/latest-openui5-landing-page/tree/master/api'
+            }
+        };
+    }
+
+    context.res = response;
     context.done();
 };
 
